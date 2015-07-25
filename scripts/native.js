eef95857c1cb5ee9bd18b25c2c73392d461f7455
Satoshi Ebisawa <e.satoshi@gmail.com>
2015-07-25 18:02:34 +0900

    Add timestamp layer

--- scripts/native.js
+++ scripts/native.js
@@ -26,10 +26,37 @@ function setFrame(frameId) {
   frame.onload = function() {
     ctx.drawImage(frame, 0, 0, 550, 309);
   };
+  setTimestamp();
+}
+
+function setTimestamp() {
+  var canvas = document.getElementById('timestamp');
+  var ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, 550, 309);
+
+  ctx.font = "16px 'Roboto'";
+  ctx.fillStyle = "rgb(255, 78, 0)";
+  ctx.textAlign =  "right";
+  var margin = 10;
+  ctx.shadowBlur = 2;
+  ctx.shadowColor = "rgba(0, 0, 0, .6)";
+  ctx.shadowOffsetX = 0;
+  ctx.shadowOffsetY = 1;
+  ctx.fillText(timestamp(), 550 - margin, 309 - margin);
 }
 
 function clearFrame() {
   var canvas = document.getElementById('frame');
   var ctx = canvas.getContext('2d');
   ctx.clearRect(0, 0, 550, 309);
+}
+
+function timestamp() {
+  // 2015.7.25
+  var d = new Date();
+  var year  = d.getFullYear();
+  var month = d.getMonth() + 1;
+  var date  = d.getDate();
+
+  return year + '.' + month + '.' + date;
 }
