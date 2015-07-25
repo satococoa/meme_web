699b527d5d8d50873d4e2a9aedd8017f917921bc
Satoshi Ebisawa <e.satoshi@gmail.com>
2015-07-25 15:17:36 +0900

    Setup canvas size (550x309)

--- scripts/native.js
+++ scripts/native.js
@@ -7,12 +7,12 @@ function capture(data) {
   img.src = prefix+data;
 
   img.onload = function(){
-    ctx.drawImage(img, 0, 0, 320, 320);
+    ctx.drawImage(img, 0, 0, 550, 309);
   };
 }
 
 function clear() {
   var canvas = document.getElementById('frame');
   var ctx = canvas.getContext('2d');
-  ctx.clearRect(0, 0, 320, 320);
+  ctx.clearRect(0, 0, 550, 309);
 }
